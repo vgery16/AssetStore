678b6a4926f131d5266cf1eec31a82ab43691f62
vgery16 <vgery16@yahoo.com>
2018-03-30 12:56:47 +0200

    Basic ColliderAction Created
    What next ?
    - Inspector for ColliderActions

---
+++ StoreProject/Assets/Scripts/ColliderActions.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColliderActions : MonoBehaviour {
+
+    [HideInInspector]
+    public string ObservedTag;
+    [HideInInspector]
+    public UnityEvent OnTriggerEnterEvent;
+    [HideInInspector]
+    public UnityEvent OnTriggerExitEvent;
+    [HideInInspector]
+    public UnityEvent OnTriggerStayEvent;
+
+    private Collider Player;
+
+    #region Collider Calbacks
+    void OnTriggerEnter(Collider other)
+    {
+        if (ObservedTag.Equals(other.tag))
+        {
+            Player = other;
+            OnTriggerEnterEvent.Invoke();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (ObservedTag.Equals(other.tag)) OnTriggerExitEvent.Invoke();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (ObservedTag.Equals(other.tag))
+        {
+            Player = other;
+            OnTriggerStayEvent.Invoke();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    #endregion
+
+    // If I want to destroy everything with ObeservedTag
+    public void DestroyPlayer(float time)
+    {
+        Destroy(Player.gameObject, time);
+    }
+}
